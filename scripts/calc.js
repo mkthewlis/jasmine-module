59403e3f71de1fd5609966b34a0c7cabe71490cd
mkthewlis <mkthewlis@gmail.com>
2020-06-04 12:44:32 +0000

    docs: added three examples of how Jasmine can be used to check for bugs and problems in code, two of which are commented out for future reference

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,44 @@
+function fizzBuzz(number){
+    if(number % 3 === 0 && number % 5 === 0){
+        return 'FizzBuzz';
+    }
+    else if(number % 3 === 0){
+        return 'Fizz';
+    }
+    else if(number % 5 === 0){
+        return 'Buzz';
+    } else {
+        return number;
+    }
+};
+
+
+
+//WHAT CAN I DRINK?
+/*function whatCanIDrink(age){
+    if(age < 0){
+        return "Sorry. I can’t tell what drink because that age is incorrect!";
+    }
+    else if(age < 14){
+        return "Drink Toddy";
+    }
+    else if(age < 18 && age >=14){
+        return "Drink Coke";
+    }
+    else if(age < 21 && age >=18){
+        return "Drink Beer";
+    }
+    else if(age < 130){
+        return "Drink Whisky";
+    } else {
+        return "Sorry. I can’t tell what drink because that age is incorrect!"
+    }
+};
+*/
+
+
+//CALCULATOR EXAMPLE BELOW
+/*
 Calculator = function() {
     this.value = 0;
 };
@@ -9,3 +50,5 @@ Calculator.prototype.add = function(number) {
         alert("Error!");
     }
 };
+
+*/
